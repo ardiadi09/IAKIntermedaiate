f86b242e67f1953e1718842ac43bd9da41d8852f
Ari Ardiadi <ariardiadi@gmail.com>
2017-08-12 15:26:58 +0800

    Commit Comment

---
+++ app/src/main/java/com/example/android/iak2017intermediate/rest/ApiClient.java
@@ -0,0 +1,21 @@
+package com.example.android.iak2017intermediate.rest;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+public class ApiClient {
+    private static final String BASE_URL = "https://newsapi.org/v1";
+    private Retrofit mRetrofit;
+
+    public Retrofit getRetrofitClient(){
+        if(mRetrofit == null){
+            mRetrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return mRetrofit;
+    }
+}
